da1ce140f6ebe1ae0672296ff0601c2bbba61fda
kikye04040 <kikye04040@gmail.com>
2025-02-04 10:46:56 +0900

    함수 정의와 사용

--- 1week/PythonPrac/hello.py
+++ 1week/PythonPrac/hello.py
@@ -1,31 +1,7 @@
-# dictionary
+# function
 
-bob = {
-    "name":"bob",
-    "age":22,
-    "address":"Seoul"
-}
+def sum(a, b):
+    return a + b
 
-print(bob["name"])
-print(bob["age"])
-print(bob["address"])
-
-people = [
-{
-    "name":"bab",
-    "age":22,
-    "address":"Seoul"
-},
-{
-    "name":"bib",
-    "age":22,
-    "address":"Seoul"
-},
-{
-    "name":"bub",
-    "age":22,
-    "address":"Seoul"
-}
-]
-
-print(people[0]["name"])
+print(sum(1, 2))
+print(sum(3, 4))
