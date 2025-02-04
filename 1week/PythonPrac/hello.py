87323606428da99f5d19cd3a6c215ad720123100
kikye04040 <kikye04040@gmail.com>
2025-02-04 11:09:24 +0900

    함수, 조건문, 반복문

--- 1week/PythonPrac/hello.py
+++ 1week/PythonPrac/hello.py
@@ -1,18 +1,17 @@
-# 반복문
+# 조건문과 반복문
 
-fruitsEnglish = ['apple','banana','orange']
+people = [{'name': 'bob', 'age': 20},
+          {'name': 'carry', 'age': 38},
+          {'name': 'john', 'age': 7},
+          {'name': 'smith', 'age': 17},
+          {'name': 'ben', 'age': 27}]
 
-for fruit in fruitsEnglish:
-    print(fruit)
+def get_age(name):
+    for person in people:
+        if person['name'] == name:
+            return person['age']
+    return "해당 이름이 없습니다."
 
-def count_fruits(fruit_name):
-    fruits = ['사과','배','배','감','수박','귤','딸기','사과','배','수박']
-    count = 0
-
-    for f in fruits:
-        if f == fruit_name:
-            count += 1
-
-    return count
-
-print(count_fruits('사과'))
+print(get_age('bob')) # 20
+print(get_age('john')) # 7
+print(get_age('min')) # 해당 이름이 없습니다.
