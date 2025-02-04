88e89dca1e07023cc25b2848a0dc0038f1979a21
kikye04040 <kikye04040@gmail.com>
2025-02-04 20:57:47 +0900

    파이썬 패키지 사용해보기
    미세먼지 API를 사용해서 구 이름이 있으면 구 이름과 미세먼지 딕셔너리를 반환하고 없을 시에는 미세먼지 수치가 없다는 멘트를 반환하는 함수 선언

--- 1week/PythonPrac/hello.py
+++ 1week/PythonPrac/hello.py
@@ -2,17 +2,22 @@ from pprint import pprint
 
 import requests # requests 라이브러리 설치 필요
 
-r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
-rjson = r.json()
+def get_gu_mise(name):
+    r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
+    rjson = r.json()
 
-pprint(rjson)
+    pprint(rjson)
 
-rows = rjson['RealtimeCityAir']['row']
-pprint(rows)
+    rows = rjson['RealtimeCityAir']['row']
+    pprint(rows)
 
-for gu in rows:
-    gu_name = gu['MSRSTE_NM']
-    gu_mise = gu['IDEX_MVL']
+    for gu in rows:
+        gu_name = gu['MSRSTE_NM']
+        gu_mise = gu['IDEX_MVL']
+        if gu_name == name:
+            return gu_mise
+    return "미세먼지 수치가 없습니다."
 
-    if gu_mise < 60:
-        print(gu_name, gu_mise)
+print(get_gu_mise("중구"))
+print(get_gu_mise("종로규"))
+print(get_gu_mise("수지구"))
