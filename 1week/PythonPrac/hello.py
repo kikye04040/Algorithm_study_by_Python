89a950526dc9f9f90fde81ee79f2858d8bb77ba0
kikye04040 <kikye04040@gmail.com>
2025-02-04 20:54:04 +0900

    파이썬 패키지 사용해보기
    미세먼지 API를 정리해서 사용해보기

--- 1week/PythonPrac/hello.py
+++ 1week/PythonPrac/hello.py
@@ -1,11 +1,18 @@
-# class
+from pprint import pprint
 
-class Person:
-    def __init__(self, name):
-        self.name = name
+import requests # requests 라이브러리 설치 필요
 
-    def sayHello(self, toWhom):
-        print(f"hello, {toWhom}. I am {self.name}")
+r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
+rjson = r.json()
 
-kihye = Person("기혜")
-kihye.sayHello("정민")
+pprint(rjson)
+
+rows = rjson['RealtimeCityAir']['row']
+pprint(rows)
+
+for gu in rows:
+    gu_name = gu['MSRSTE_NM']
+    gu_mise = gu['IDEX_MVL']
+
+    if gu_mise < 60:
+        print(gu_name, gu_mise)
