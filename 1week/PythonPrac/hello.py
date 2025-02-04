29ff2e276ac038aff3390f4c00b3acf48414a2e5
kikye04040 <kikye04040@gmail.com>
2025-02-04 11:02:01 +0900

    반복문으로 만든 함수

--- 1week/PythonPrac/hello.py
+++ 1week/PythonPrac/hello.py
@@ -5,11 +5,14 @@ fruitsEnglish = ['apple','banana','orange']
 for fruit in fruitsEnglish:
     print(fruit)
 
-fruits = ['사과','배','배','감','수박','귤','딸기','사과','배','수박']
+def count_fruits(fruit_name):
+    fruits = ['사과','배','배','감','수박','귤','딸기','사과','배','수박']
+    count = 0
 
-count = 0
+    for f in fruits:
+        if f == fruit_name:
+            count += 1
 
-for f in fruits:
-    if f == "사과":
-        count += 1
-print(count)
+    return count
+
+print(count_fruits('사과'))
