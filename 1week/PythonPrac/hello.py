c3846b9474577e06c544ba37b0eee1151728fc68
kikye04040 <kikye04040@gmail.com>
2025-02-04 10:41:14 +0900

    자료형 - list

--- 1week/PythonPrac/hello.py
+++ 1week/PythonPrac/hello.py
@@ -1,8 +1,9 @@
-# boolean
+# list
 
-name = 'bob'
-age = 14
+shop = ["수박", "사과", "참외"]
 
-isAdult = age > 19
-
-print(isAdult)
+print(shop[0]) # 리스트의 시작은 0부터
+print(shop[-1]) # 역순으로 -> 참외
+print(shop[-2]) # 사과
+print(shop[-3]) # 수박
+#print(shop[-4]) # IndexError 범위를 벗어남
