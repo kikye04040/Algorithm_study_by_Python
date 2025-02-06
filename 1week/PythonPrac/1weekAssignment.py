24827d789a1eed2ee7cab3107275f4656e07f2ee
kikye04040 <kikye04040@gmail.com>
2025-02-06 20:54:10 +0900

    1week Assignment
    1번: 팰린드롬 확인 알고리즘 작성

---
+++ 1week/PythonPrac/1weekAssignment.py
@@ -0,0 +1,16 @@
+str1 = "radar"
+str2 = "Python"
+
+def is_palindrome(s):
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+print(is_palindrome(str1))
+print(is_palindrome(str2))
