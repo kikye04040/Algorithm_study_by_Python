e02c8c9ce0fce985ac765878a69f3eb6395f7001
kikye04040 <kikye04040@gmail.com>
2025-02-18 21:37:53 +0900

    알고리즘 - 힙정렬

--- 4week/test.py
+++ 4week/test.py
@@ -1,4 +1,4 @@
-from prac import bubblesort, selectionsort, insertionsort, quicksort, merge, mergesort
+from prac import bubblesort, selectionsort, insertionsort, quicksort, merge, mergesort, heapsort
 
 
 # 버블 정렬
@@ -38,3 +38,8 @@ assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 6) == [1, 2, 2, 3, 3, 3, 5]
 assert merge([1,2,3,5],[4,6,7,8]) == [1,2,3,4,5,6,7,8]
 assert mergesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert mergesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
+
+
+# 힙 정렬
+
+assert heapsort([1, 4, 2, 10, 5]) == [1, 2, 4, 5, 10]
