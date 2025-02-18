0524bf3dd1470d8097d60d027157a3c334f12354
kikye04040 <kikye04040@gmail.com>
2025-02-18 21:08:58 +0900

    알고리즘 - 삽입정렬

--- 4week/prac.py
+++ 4week/prac.py
@@ -26,3 +26,18 @@ def selectionsort(lst):
             lst[minimun], lst[iter] = lst[iter], lst[minimun]
 
     return lst
+
+
+# 삽입 정렬
+
+def insertionsort(lst):
+    # 0번째 요소는 이미 정렬되어있으니, 1번째 ~ lst(len)-1 번째를 정렬하면 된다.
+    for cur in range(1, len(lst)):
+        # 비교지점이 cur-1 ~ 0(=cur-cur)까지 내려간다.
+        for delta in range(1, cur + 1):
+            cmp = cur - delta
+            if lst[cmp] > lst[cmp + 1]:
+                lst[cmp], lst[cmp + 1] = lst[cmp + 1], lst[cmp]
+            else:
+                break
+    return lst
--- 4week/test.py
+++ 4week/test.py
@@ -1,4 +1,4 @@
-from prac import bubblesort, selectionsort
+from prac import bubblesort, selectionsort, insertionsort
 
 
 # 버블 정렬
@@ -11,3 +11,9 @@ assert bubblesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
 
 assert selectionsort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert selectionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
+
+
+# 삽입 정렬
+
+assert insertionsort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
+assert insertionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
