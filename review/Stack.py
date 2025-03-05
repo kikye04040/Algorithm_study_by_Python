7493dd990e5fc7eeb6ac15eb497212e8af529a60
kikye04040 <kikye04040@gmail.com>
2025-03-05 22:39:40 +0900

    250305 스택 정답

--- review/Stack.py
+++ review/Stack.py
@@ -8,7 +8,16 @@ class Stack:
         self.top = None
 
     def push(self, val):
+        self.top = Node(val, self.top)
 
     def pop(self):
+        if self.top is None:
+            return None
+
+        node = self.top
+        self.top = self.top.next
+
+        return node.val
 
     def is_empty(self):
+        return self.top is None
