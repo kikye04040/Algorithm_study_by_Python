252d611a2ef3eedda0c2440787fe311bd6b916e1
kikye04040 <kikye04040@gmail.com>
2025-03-08 21:47:30 +0900

    250308 큐 암기 성공

---
+++ review/Queue.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Queue:
+    def __init__(self):
+        self.front = None
+
+    def push(self, val):
+        if not self.front:
+            self.front = Node(val)
+            return
+        node = self.front
+        while node.next:
+            node = node.next
+        node.next = Node(val)
+
+    def pop(self):
+        if not self.front:
+            return None
+        node = self.front
+        self.front = self.front.next
+        return node.val
+
+    def is_empty(self):
+        return self.front is None
